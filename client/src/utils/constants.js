c47c50f154a7a87481927ecfe47e168b63aa3103
Adi-204 <adiboghawala@gmail.com>
2025-04-05 12:53:48 +0530

    chore: add .env file to store secret

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -13,6 +13,6 @@ export const contractAbi = abi;
 // ! ||                                   Pinata IPFS                                  ||
 // ! ||--------------------------------------------------------------------------------||
 
-export const API_Key =  'fabdd4370cb5a2c508e8'
-export const API_Secret=  'd83c3018157a82f149e6e31651ba6ef8039587ee14fdf5ff4581866700d3b740'
-export const JWT= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJjMjFlMDBmNC1kMDQ4LTQyMjgtYWJlZS0wNGE4YTNjYTMzYzEiLCJlbWFpbCI6ImFkaWJvZ2hhd2FsYUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZmFiZGQ0MzcwY2I1YTJjNTA4ZTgiLCJzY29wZWRLZXlTZWNyZXQiOiJkODNjMzAxODE1N2E4MmYxNDllNmUzMTY1MWJhNmVmODAzOTU4N2VlMTRmZGY1ZmY0NTgxODY2NzAwZDNiNzQwIiwiZXhwIjoxNzc0MjkwMDAzfQ.sb6hkYZiPpfMtBVVAlmdWETYI8LFrKxV5UP02xfFocI'
+export const API_Key = import.meta.env.API_Key;
+export const API_Secret=  import.meta.env.API_Secret;
+export const JWT= import.meta.env.JWT;
